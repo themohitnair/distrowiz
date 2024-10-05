3bdb7421993abed91e8e6e124642ef81baec4cf3
Mohit <themohitnair@protonmail.com>
2024-10-05 13:28:38 +0530

    added questions: preliminary, will edit

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -5,8 +5,10 @@ interface QuestionProps {
 
 const Question: React.FC<QuestionProps> = ({ question, options }) => {
     return (
-        <div className="question border border-white px-5 py-5 mt-10 w-4/5 m-auto rounded-sm text-2xl">
-            <div>{question}</div>
+        <div className="question border border-white px-5 py-5 mt-10 w-4/5 m-auto rounded-sm">
+            <div className="text-2xl">
+                {question}
+            </div>
             <ul className="pt-5">
                 {options.map((option, index) => (
                     <li key={index} className="option border-white text-xl rounded-sm border mt-3 px-3 py-2 hover:bg-white hover:text-black">
