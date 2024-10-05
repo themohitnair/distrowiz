6ae6558beccd83f379b6fa8cf4e22fe1943f95a2
Mohit <themohitnair@protonmail.com>
2024-10-05 17:45:23 +0530

    Added more distros, brought it up to 18

--- src/components/Distros.tsx
+++ src/components/Distros.tsx
@@ -8,22 +8,28 @@ interface DistroInfo {
 
 const Distros: React.FC = () => {
     const distros: { [key: string]: DistroInfo } = {
-        Ubuntu: { path: 'distro-icons/ubuntu.png', subreddit: 'ubuntu' },
-        Fedora: { path: 'distro-icons/fedora.png', subreddit: 'fedora' },
         Arch: { path: 'distro-icons/arch.png', subreddit: 'archlinux' },
-        Debian: { path: 'distro-icons/debian.png', subreddit: 'debian' },
-        CentOS: { path: 'distro-icons/centos.png', subreddit: 'centos' },
+        Ubuntu: { path: 'distro-icons/ubuntu.png', subreddit: 'Ubuntu' },
+        Fedora: { path: 'distro-icons/fedora.png', subreddit: 'Fedora' },
         Kali: { path: 'distro-icons/kali.png', subreddit: 'kalilinux' },
         Mint: { path: 'distro-icons/mint.png', subreddit: 'linuxmint' },
+        Debian: { path: 'distro-icons/debian.png', subreddit: 'debian' },
+        PopOS: { path: 'distro-icons/pop.png', subreddit: 'pop_os' },        
+        Manjaro: { path: 'distro-icons/manjaro.png', subreddit: 'ManjaroLinux' },
+        RedHat: { path: 'distro-icons/redhat.png', subreddit: 'redhat' },        
         NixOS: { path: 'distro-icons/nixos.png', subreddit: 'nixos' },
-        Zorin: { path: 'distro-icons/zorin.png', subreddit: 'zorinos' },
-        PopOS: { path: 'distro-icons/pop.png', subreddit: 'pop_os' },
         Gentoo: { path: 'distro-icons/gentoo.png', subreddit: 'gentoo' },
-        OpenSUSE: { path: 'distro-icons/suse.png', subreddit: 'openSUSE' }
+        ElementaryOS: { path: 'distro-icons/elementary.png', subreddit: 'elementaryos' },
+        CentOS: { path: 'distro-icons/centos.png', subreddit: 'centos' },
+        EndeavourOS: { path: 'distro-icons/endeavour.png', subreddit: 'EndeavourOS' },  
+        Qubes: { path: 'distro-icons/qubes.png', subreddit: 'Qubes' },   
+        Void: { path: 'distro-icons/void.png', subreddit: 'voidlinux' },
+        Zorin: { path: 'distro-icons/zorin.png', subreddit: 'zorinos' },
+        OpenSUSE: { path: 'distro-icons/suse.png', subreddit: 'openSUSE' },       
     };
 
     return (
-    <div className="distros w-3/4 m-auto mt-16 grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-6 gap-6">
+    <div className="distros w-4/5 m-auto mt-16 grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-9 gap-6">
         {Object.entries(distros).map(([distro, info]) => (
         <Distro key={distro} distro={distro} path={info.path} subreddit={info.subreddit} />
         ))}
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,17 @@
-const Results: React.FC = () => {
+interface Result {
+    iconPath: string
+    distro: string
+}
+
+const Results: React.FC<Result> = ({iconPath, distro}) => {
     return (
-        <div className="results">
+        <div className="results flex grid-cols-2">
+            <div className="icon">
+                <img src="distro-icons/ubuntu.png" alt={distro} />
+            </div>
+            <div className="about">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit quisquam atque rerum quod, delectus libero tenetur possimus natus dicta dolore veniam at asperiores laboriosam quis fuga velit distinctio a numquam.
+            </div>            
         </div>
     )
 }
