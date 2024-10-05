9b4b592de9747160fc6ed5723408b3b578c9aadc
Mohit <themohitnair@protonmail.com>
2024-10-05 18:13:52 +0530

    Reduced distro list to 15, added some key About points, and changed button styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,8 @@
 const About: React.FC = () => {
     return (
         <div className="about mt-12 w-3/4 m-auto text-xl">
-            This quiz is designed for those who are switching to Linux or are new to it. This quiz will help you choose from the 12 most popular Linux distributions. 
+            This quiz is designed for those who are switching to Linux or are new to it. This quiz will help you choose from the 15 most popular Linux distributions. <br/>
+            <b>Note:</b> The intention of this quiz is not to prove any distribution to be better than the other. Linux enthusiasts who are disappointed with the results can try the distribution they like. This project just intends to guide Linux newbies to some Linux distro, and not leave them in uncertainty before choosing one.
         </div>        
     )
 }
--- src/components/Distros.tsx
+++ src/components/Distros.tsx
@@ -16,20 +16,17 @@ const Distros: React.FC = () => {
         Debian: { path: 'distro-icons/debian.png', subreddit: 'debian' },
         PopOS: { path: 'distro-icons/pop.png', subreddit: 'pop_os' },        
         Manjaro: { path: 'distro-icons/manjaro.png', subreddit: 'ManjaroLinux' },
-        RedHat: { path: 'distro-icons/redhat.png', subreddit: 'redhat' },        
         NixOS: { path: 'distro-icons/nixos.png', subreddit: 'nixos' },
         Gentoo: { path: 'distro-icons/gentoo.png', subreddit: 'gentoo' },
         ElementaryOS: { path: 'distro-icons/elementary.png', subreddit: 'elementaryos' },
         CentOS: { path: 'distro-icons/centos.png', subreddit: 'centos' },
         EndeavourOS: { path: 'distro-icons/endeavour.png', subreddit: 'EndeavourOS' },  
-        Qubes: { path: 'distro-icons/qubes.png', subreddit: 'Qubes' },   
-        Void: { path: 'distro-icons/void.png', subreddit: 'voidlinux' },
         Zorin: { path: 'distro-icons/zorin.png', subreddit: 'zorinos' },
         OpenSUSE: { path: 'distro-icons/suse.png', subreddit: 'openSUSE' },       
     };
 
     return (
-    <div className="distros w-4/5 m-auto mt-16 grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-9 gap-6">
+    <div className="distros w-3/4 m-auto mt-16 grid grid-cols-6 lg:grid-cols-5 gap-6">
         {Object.entries(distros).map(([distro, info]) => (
         <Distro key={distro} distro={distro} path={info.path} subreddit={info.subreddit} />
         ))}
